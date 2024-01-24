84c9582b4b5eef1536f725d86451e8f4c4b2a81c
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-01-23 19:06:21 -0800

    class hoisting added

---
+++ javascript/intermediate/1_class/class_hoisting/class_hoisting_declaration_2.js
@@ -0,0 +1,11 @@
+
+class Hobbit {
+  constructor(height, weight) {
+    this.height = height;
+    this.weight = weight;
+  }}
+var Frodo = new Hobbit();
+Frodo.height = 100;
+Frodo.weight = 300;
+console.log(Frodo); // Output: ReferenceError: Hobbit is not defined
+
