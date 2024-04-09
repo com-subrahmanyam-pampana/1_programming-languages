3b62bd89012dde900341f556eb8cb85e29a72c89
com-subrahmanyam-pampana <130865446+com-subrahmanyam-pampana@users.noreply.github.com>
2024-04-09 08:40:13 -0700

    working state

--- typescript/src/basics/b_datatypes/object/index_signatures/without_index_signatures.ts
+++ typescript/src/basics/b_datatypes/object/index_signatures/without_index_signatures.ts
@@ -1,28 +1,28 @@
 /*You have 2 objects that describe the salary 
-of 2 software developers:*/
+of 2 US employee :*/
 
 const salary1 = {
-    baseSalary: 100_000,
+    baseSalary: 10000,
     yearlyBonus: 20_000
-  };
-  
-  const salary2 = {
+};
+
+const salary2 = {
     contractSalary: 110_000
-  };
+};
 
 /*You want to implement a function that returns the 
 total remuneration based on the salary object: */
-  function totalSalary(salaryObject: any) {
+function totalSalary(salaryObject: any) {
     let total = 0;
     for (const name in salaryObject) {
-      total += salaryObject[name];
+        total += salaryObject[name];
     }
     return total;
-  }
-  
-  console.log(totalSalary(salary1)); // => 120_000
-  console.log(totalSalary(salary2)); // => 110_000
+}
 
-  //How would you annotate salaryObject parameter of totalSalary() function to accept objects with key as string and value as number?
+console.log(totalSalary(salary1)); // => 120_000
+console.log(totalSalary(salary2)); // => 110_000
+
+//How would you annotate salaryObject parameter of totalSalary() function to accept objects with key as string and value as number?
 
 //The answer is to use an index signature!
